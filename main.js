89a3879b24990395cf3f617044b44db5d226a886
Michael Keogh <brookemossburn@gmail.com>
2017-06-21 14:04:35 +1200

    Add calculator

---
+++ main.js
@@ -0,0 +1,31 @@
+var screen = document.getElementById("calcInput");
+var answer = document.getElementById('calcAnswer')
+
+function writeInput(x) {
+ screen.value += x;
+}
+
+function clearInput() {
+  var clearScr = document.getElementById('calcInput')
+  clearScr.value = "";
+  var clearAns = document.getElementById('calcAnswer')
+  clearAns.value = "";
+}
+
+function clearInputByOne() {
+  var subByOne = document.getElementById('calcInput').value;
+  document.getElementById('calcInput').value =
+  subByOne.substr(0,subByOne.length - 1);
+}
+
+function sum() {
+  x = screen.value;
+  x = eval(x);
+  answer.value = x;
+}
+
+function calcHistory () {
+  x = screen.value;
+  x = eval(x);
+  screen.value = x;
+}
